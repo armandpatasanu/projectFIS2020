67b504cc1271162ba6eff72e0a479124a74fc761
Armand <sebapatasanu@yahoo.com>
2020-05-31 17:14:08 +0300

    FIS-2/ Created a GUI for user login

--- src/main/java/org/loose/oose/fis/lab/project/Main.java
+++ src/main/java/org/loose/oose/fis/lab/project/Main.java
@@ -1,38 +1,17 @@
 package org.loose.oose.fis.lab.project;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    Button b1;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello Student");
-        b1=new Button();
-        b1.setText("Intrebare Vitala");
-        b1.setOnAction(e ->{
-            boolean result= ConfirmBox.display("Alt window","Iti place FIS?"); // AlertBox.display
-            if(result==true)
-                System.out.println("Bravo");
-            else
-                System.out.println("Esti ciudat");
-        });
 
-        StackPane layout=new StackPane();
-        layout.getChildren().add(b1);
-
-        Scene s1= new Scene(layout,300, 275);
-        primaryStage.setScene(s1);
-        primaryStage.show();
+        Stage loginStage = Tools.createLoginStage(primaryStage);
+        loginStage.show();
     }
-
 }
---
+++ src/main/java/org/loose/oose/fis/lab/project/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package org.loose.oose.fis.lab.project.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class LoginController {
+    public void handleLoginAction(ActionEvent event) {
+    }
+
+    public void openCreateAccountHandler(ActionEvent event) {
+    }
+
+    public void closingLogin(MouseEvent event) {
+        System.exit(0);
+    }
+}
