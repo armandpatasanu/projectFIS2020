50019272c83f79cf4bb85611049d2771494cf225
Armand <sebapatasanu@yahoo.com>
2020-06-02 11:59:28 +0300

    FIS-15/Creating login functions to check users credentials

--- src/main/java/org/loose/oose/fis/lab/project/controllers/LoginController.java
+++ src/main/java/org/loose/oose/fis/lab/project/controllers/LoginController.java
@@ -1,16 +1,70 @@
 package org.loose.oose.fis.lab.project.controllers;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import static org.loose.oose.fis.lab.project.services.UserService.*;
 
-public class LoginController {
-    public void handleLoginAction(ActionEvent event) {
-    }
+import org.loose.oose.fis.lab.project.Tools;
+import org.loose.oose.fis.lab.project.services.UserService;
 
-    public void openCreateAccountHandler(ActionEvent event) {
-    }
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private Text loginErrorText;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    public Button logInButton;
+    @FXML
+    private Label lblLogin;
+
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private AnchorPane anchorPane;
+
 
     public void closingLogin(MouseEvent event) {
         System.exit(0);
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void handleLoginAction() throws Exception {
+
+    }
+
+    public void openCreateAccountHandler(ActionEvent event) {
     }
 }
---
+++ src/main/java/org/loose/oose/fis/lab/project/services/UserService.java
@@ -0,0 +1,72 @@
+package org.loose.oose.fis.lab.project.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.FileUtils;
+import org.loose.oose.fis.lab.project.model.User;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Objects;
+
+public class UserService {
+
+    private static List<User> users;
+    private static final Path USERS_PATH = FileSystemService.getPathToFile("config", "json/users.json");
+
+    public static void loadUsersFromFile() throws IOException {
+
+        if (!Files.exists(USERS_PATH)) {
+            FileUtils.copyURLToFile(UserService.class.getClassLoader().getResource("json/users.json"), USERS_PATH.toFile());
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        users = objectMapper.readValue(USERS_PATH.toFile(), new TypeReference<List<User>>() {
+        });
+    }
+
+    public static boolean checkLoginUsername(String username) {
+        for (User user : users) {
+            if (Objects.equals(username, user.getUsername()))
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean checkLoginPassword(String password, String username) {
+        String newpassword = encodePassword(username, password);
+
+        for (User user : users) {
+            if (newpassword.equalsIgnoreCase(user.getPassword()))
+                return true;
+        }
+        return false;
+    }
+
+    private static String encodePassword(String salt, String password) {
+        MessageDigest md = getMessageDigest();
+        md.update(salt.getBytes(StandardCharsets.UTF_8));
+
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        // This is the way a password should be encoded when checking the credentials
+        return new String(hashedPassword, StandardCharsets.UTF_8)
+                .replace("\"", ""); //to be able to save in JSON format
+    }
+
+    private static MessageDigest getMessageDigest() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-512 does not exist!");
+        }
+        return md;
+    }
+}
