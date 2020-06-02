833ceb88f9c97cedf5b0113193c48589ac4f54c6
Armand <sebapatasanu@yahoo.com>
2020-06-02 13:23:49 +0300

    FIS-10/Created a GUI for user registration

---
+++ src/main/java/org/loose/oose/fis/lab/project/controllers/RegisterController.java
@@ -0,0 +1,11 @@
+package org.loose.oose.fis.lab.project.controllers;
+
+import javafx.event.ActionEvent;
+
+public class RegisterController {
+    public void createAccountHandler(ActionEvent event) {
+    }
+
+    public void backToLoginHandler(ActionEvent event) {
+    }
+}
